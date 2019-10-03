cd71ab8888737ec271400e0644b8a01af17ed7db
Antoine Weber <aweb33@icloud.com>
2019-10-03 10:07:55 +0200

    file encryption and decryption

--- Reblock/symmetric.playground/Contents.swift
+++ Reblock/symmetric.playground/Contents.swift
@@ -1,9 +1,9 @@
 import Foundation
 import CryptoKit
 
-let string = "Hello World!"
+let string = "Hello World!\n"
 let data = string.data(using: .utf8)!
-
+let clearText = data.base64EncodedString()
 
 let key = SymmetricKey(size: .bits256)
 let sealedBox = try! ChaChaPoly.seal(data, using: key)
@@ -11,5 +11,18 @@ let encryptedText = sealedBox.ciphertext
 encryptedText.base64EncodedString()
 
 let returnedData = try! ChaChaPoly.open(sealedBox, using: key)
+returnedData.base64EncodedString()
 let returnedString = String(data: returnedData, encoding: .utf8)!
+ 
 
+
+
+
+
+let path = Bundle.main.path(forResource: "randomText", ofType: "txt")!
+let fileData = FileManager.default.contents(atPath: path)!
+fileData.base64EncodedString()
+let sealedBox2 = try! ChaChaPoly.seal(fileData, using: key)
+sealedBox2.ciphertext.base64EncodedString()
+let returnedFileData = try! ChaChaPoly.open(sealedBox2, using: key)
+let stringreturned = String(data: returnedFileData, encoding: .utf8)
